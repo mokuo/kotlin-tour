6e048ae6eba47816ac14d7ee0deffa740e8bc03a
mokuo <tennis10988.yk@gmail.com>
2026-04-23 17:19:34 +0900

    Kotlin Tour の Collections の Exercise 1
    - https://kotlinlang.org/docs/kotlin-tour-collections.html#exercise-1

---
+++ src/Collections/Ex1.kt
@@ -0,0 +1,8 @@
+package Collections
+
+fun main() {
+    val greenNumbers = listOf(1, 4, 23)
+    val redNumbers = listOf(17, 2)
+    // Write your code here
+    println(greenNumbers.count() + redNumbers.count())
+}
