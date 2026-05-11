fea9bdd63093e5a45b7fbbc52f3ca7250552c1e4
mokuo <tennis10988.yk@gmail.com>
2026-05-11 09:50:07 +0900

    fin https://kotlinlang.org/docs/kotlin-tour-functions.html#lambda-expressions-practice

---
+++ src/beginner/functions/lambda_expressions/ex2.kt
@@ -0,0 +1,11 @@
+package beginner.functions.lambda_expressions
+
+fun repeatN(n: Int, action: () -> Unit) {
+    for (i in 1..n) {
+        action()
+    }
+}
+
+fun main() {
+    repeatN(5) { println("Hello") }
+}
