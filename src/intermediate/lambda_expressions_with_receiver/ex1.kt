aaa01d172721ee3788ad23d8ae741cbcf46e7633
mokuo <tennis10988.yk@gmail.com>
2026-05-20 17:28:55 +0900

    fin https://kotlinlang.org/docs/kotlin-tour-intermediate-lambdas-receiver.html#lambda-receivers-exercise-1

---
+++ src/intermediate/lambda_expressions_with_receiver/ex1.kt
@@ -0,0 +1,13 @@
+package intermediate.lambda_expressions_with_receiver
+
+fun fetchData(callback: StringBuilder.() -> Unit) {
+    val builder = StringBuilder("Data received")
+    builder.callback()
+}
+
+fun main() {
+    fetchData {
+        append(" - Processed")
+        println(toString())
+    }
+}
