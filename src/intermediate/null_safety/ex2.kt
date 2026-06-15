2a9c7cee145b2d9b5fe01285bc92b4f35a964747
mokuo <tennis10988.yk@gmail.com>
2026-06-15 17:48:13 +0900

    fin https://kotlinlang.org/docs/kotlin-tour-intermediate-null-safety.html#null-safety-exercise-2

---
+++ src/intermediate/null_safety/ex2.kt
@@ -0,0 +1,24 @@
+package intermediate.null_safety
+
+data class Subscription(val name: String, val isActive: Boolean)
+
+fun getActiveSubscription(subscriptions: List<Subscription>): Subscription? =
+    subscriptions.singleOrNull { it.isActive } // Write your code here
+
+fun main() {
+    val userWithPremiumPlan = listOf(
+        Subscription("Basic Plan", false),
+        Subscription("Premium Plan", true)
+    )
+
+    val userWithConflictingPlans = listOf(
+        Subscription("Basic Plan", true),
+        Subscription("Premium Plan", true)
+    )
+
+    println(getActiveSubscription(userWithPremiumPlan))
+    // Subscription(name=Premium Plan, isActive=true)
+
+    println(getActiveSubscription(userWithConflictingPlans))
+    // null
+}
